7338e032fad0fbdfce28351ff68a6d1a138e81c8
sanketjirapure1 <76209110+sanketjirapure1@users.noreply.github.com>
2023-03-07 20:41:54 +0530

    Add files via upload
    footerupdate.

---
+++ src/assets/index.js
@@ -0,0 +1,38 @@
+// import logo from "./images/logo.png";
+// import bannerImg from "./images/bannerImg.png";
+// import sgjlogo from "./images/sgjlogo.png";
+
+  import sanket11pic from "./images/sanket11pic.png";
+// import sanketprofile from "./images/sanketprofile.png";
+
+
+// import sanket11pic from "./images/sanket222pic-removebg-preview.png";
+// import sanket11pic from "./images/profile-pic (14).png";
+// import sanket11pic from "./images/profile-pic (10).png";
+
+
+
+// import projectOne from "./images/projects/projectOne.jpg";
+import projectOne from "./images/projects/bagetcalculator.png";
+// import projectTwo from "./images/projects/projectTwo.jpg";
+import projectTwo from "./images/projects/todobackbone.png";
+// import projectThree from "./images/projects/projectThree.jpeg";
+// import testimonialOne from "./images/testmonial/testimonialOne.png";
+// import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+// import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.png";
+
+export {
+   //logo,
+  // bannerImg,
+  // sgjlogo,
+  // sanketprofile,
+  sanket11pic,
+  projectOne,
+  projectTwo,
+  // projectThree,
+  // testimonialOne,
+  // testimonialTwo,
+  // quote,
+  contactImg,
+};
