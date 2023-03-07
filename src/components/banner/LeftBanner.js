67cae6e14cc2a7c2cbee9d0beea3d75796d5c2a4
sanketjirapure1 <76209110+sanketjirapure1@users.noreply.github.com>
2023-03-07 21:15:34 +0530

    Update LeftBanner.js

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -46,25 +46,25 @@ const LeftBanner = () => {
             Find me in
           </h2>
           <div className="flex gap-4">
-            <a href="https://linkedin.com/in/sanket-jirapure1" target="_blank">
+            <a href="https://linkedin.com/in/sanket-jirapure1" target="_blank" rel="noreferrer">
               <span className="bannerIcon">
                 <FaLinkedinIn />
               </span>
             </a>
 
-            <a href="https://instagram.com/sanket_jirapure" target="_blank">
+            <a href="https://instagram.com/sanket_jirapure" target="_blank" rel="noreferrer">
               <span className="bannerIcon">
                 <FaInstagram />
               </span>
             </a>
 
-            <a href="https://facebook.com/jirapure1sanket" target="_blank">
+            <a href="https://facebook.com/jirapure1sanket" target="_blank" rel="noreferrer">
               <span className="bannerIcon">
                 <FaFacebookF />
               </span>
             </a>
 
-            <a href="https://twitter.com/sanket_jirapure" target="_blank">
+            <a href="https://twitter.com/sanket_jirapure" target="_blank" rel="noreferrer">
               <span className="bannerIcon">
                 <FaTwitter />
               </span>
